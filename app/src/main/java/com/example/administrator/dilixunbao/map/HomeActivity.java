a0547eb50273bc50bfe95501fa106f4e440aee73
CWIICW <2801035008@qq.com>
2017-08-07 21:48:26 +0800

    地图定位完成

--- app/src/main/java/com/example/administrator/dilixunbao/map/HomeActivity.java
+++ app/src/main/java/com/example/administrator/dilixunbao/map/HomeActivity.java
@@ -3,6 +3,7 @@ package com.example.administrator.dilixunbao.map;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -96,6 +97,7 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
                 finish();
                 break;
         }
+        mDrawerLayout.closeDrawer(GravityCompat.START);
         return false;
     }
 }
--- app/src/main/java/com/example/administrator/dilixunbao/net/NetRequest.java
+++ app/src/main/java/com/example/administrator/dilixunbao/net/NetRequest.java
@@ -3,6 +3,10 @@ package com.example.administrator.dilixunbao.net;
 import com.example.administrator.dilixunbao.Register.RegisterResult;
 import com.example.administrator.dilixunbao.User;
 import com.example.administrator.dilixunbao.login.LoginResult;
+import com.example.administrator.dilixunbao.treasure.Area;
+import com.example.administrator.dilixunbao.treasure.Treasure;
+
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
@@ -20,4 +24,9 @@ public interface NetRequest {
     //注册
     @POST("/Handler/UserHandler.ashx?action=register")
     Call<RegisterResult> register(@Body User user);
+
+    //获取宝藏信息
+    @POST("/Handler/\n" +
+            "TreasureHandler.ashx?action=show")
+    Call<List<Treasure>> getTreasure(@Body Area area);
 }
